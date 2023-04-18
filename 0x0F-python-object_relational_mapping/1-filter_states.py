5aa9f5c74ee991ebaa4b4b977f927db95326dccd
JoGMG <joattah3@gmail.com>
2023-04-18 02:19:25 +0100

    object relational mapping (ORM)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This script lists all `states` from the database `hbtn_0e_0_usa`.
+This script lists all `states` with a `name` starting 
+with `N` (upper N) from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
@@ -14,7 +15,9 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY 'N%' \
+                 ORDER BY states.id ASC")
     rows = cur.fetchall()
 
     for row in rows:
