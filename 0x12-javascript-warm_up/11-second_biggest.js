03e415bd9b598e9ac8bcf7b8808856f2c6e81578
JoGMG <joattah3@gmail.com>
2023-08-19 00:46:14 +0100

    simplified

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -11,6 +11,8 @@ const args = process.argv;
 if (args.length <= 3) {
   console.log(0);
 } else {
-  const arg = args.map(Number).slice(2).sort();
+  const arg = args.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
   console.log(arg[arg.length - 2]);
 }
