fd3bae6ea36094a1ea9b98375a6c6da1c40dcfc8
JoGMG <joattah3@gmail.com>
2023-08-19 12:36:47 +0100

    passing checker

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -8,4 +8,4 @@
 const fs = require('fs');
 const a = fs.readFileSync(process.argv[2], 'utf-8');
 const b = fs.readFileSync(process.argv[3], 'utf-8');
-fs.writeFileSync(process.argv[4], a + '\n' + b);
+fs.writeFileSync(process.argv[4], a + b);
