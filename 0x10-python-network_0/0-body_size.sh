fc157e7dac7c68c3f3a0b3ca55e9abb809c15047
JoGMG <joattah3@gmail.com>
2023-09-04 16:29:05 -0700

    added comments

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# Displays the byte size of the body of a response.
+# Write a Bash script that takes in a URL, sends a request to that URL,
+# and displays the size of the body of the response.
+# - The size must be displayed in bytes
+# - You have to use `curl`
 curl -s "$1" | wc -c
