ac4c61f064f0ae08491b86a499bf7ed9f4854528
JoGMG <joattah3@gmail.com>
2023-09-06 20:03:29 -0700

    peak value

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,30 +10,14 @@ def find_peak(list_of_integers):
     d_list = list_of_integers
     length = len(d_list)
     index = int(length / 2)
-    # print(index)
-    # print("full list length: {}".format(fl_length))
-    # print("full list index: {}".format(fl_index))
-    # fh_list = fl[:fl_index]
-    # print(fh_list)
-    # fh_list_length = len(fh_list)
-    # fh_list_index = int(fh_list_length / 2)
-    # print(fh_list_length)
-    # print(fh_list_index)
 
     if d_list[index - 1] < d_list[index] > d_list[index + 1]:
         return d_list[index]
-    if index < 2 and d_list[index] == d_list[index - 1]:
-        return d_list[index]
     if d_list[index] == d_list[index - 1] == d_list[index + 1]:
-        return d_list[index] if d_list[index + 2] \
-                <= d_list[index] else find_peak(d_list[:index])
+        return d_list[index] if index < 2 else find_peak(d_list[:index])
     if index == 1 and d_list[index] < d_list[index - 1]:
         return d_list[index - 1]
-    # elif d_list[index] < d_list[index + 1] > d_list[index - 1]:
-    #     return d_list[index + 1]
 
     if d_list[index + 1] > d_list[index - 1]:
-        # print(d_list[index:])
         return find_peak(d_list[index:])
-    # print(d_list[:index])
     return find_peak(d_list[:index])
