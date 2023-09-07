d903159b4684e0d554d292c0a31f5f0501bf7018
JoGMG <joattah3@gmail.com>
2023-09-06 19:33:32 -0700

    peak value

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,26 +1,39 @@
 #!/usr/bin/python3
-"""Defines a peak-finding algorithm."""
+""" Defines a function that finds a peak in a list of unsorted integers """
 
 
 def find_peak(list_of_integers):
-    """ Finds the peak in a list of integers """
+    """ A function that finds a peak in a list of unsorted integers. """
+
     if list_of_integers == []:
         return None
+    d_list = list_of_integers
+    length = len(d_list)
+    index = int(length / 2)
+    # print(index)
+    # print("full list length: {}".format(fl_length))
+    # print("full list index: {}".format(fl_index))
+    # fh_list = fl[:fl_index]
+    # print(fh_list)
+    # fh_list_length = len(fh_list)
+    # fh_list_index = int(fh_list_length / 2)
+    # print(fh_list_length)
+    # print(fh_list_index)
 
-    length = len(list_of_integers)
-    m = int(length / 2)
-    li = list_of_integers
+    if d_list[index - 1] < d_list[index] > d_list[index + 1]:
+        return d_list[index]
+    if index < 2 and d_list[index] == d_list[index - 1]:
+        return d_list[index]
+    if d_list[index] == d_list[index - 1] == d_list[index + 1]:
+        return d_list[index] if d_list[index + 2] \
+                <= d_list[index] else find_peak(d_list[:index])
+    if index == 1 and d_list[index] < d_list[index - 1]:
+        return d_list[index - 1]
+    # elif d_list[index] < d_list[index + 1] > d_list[index - 1]:
+    #     return d_list[index + 1]
 
-    if m - 1 < 0 and m + 1 >= length:
-        return li[m]
-    elif m - 1 < 0:
-        return li[m] if li[m] > li[m + 1] else li[m + 1]
-    elif m + 1 >= length:
-        return li[m] if li[m] > li[m - 1] else li[m - 1]
-
-    if li[m - 1] < li[m] > li[m + 1]:
-        return li[m]
-
-    if li[m + 1] > li[m - 1]:
-        return find_peak(li[m:])
-    return find_peak(li[:m])
+    if d_list[index + 1] > d_list[index - 1]:
+        # print(d_list[index:])
+        return find_peak(d_list[index:])
+    # print(d_list[:index])
+    return find_peak(d_list[:index])
