bae2c02a9f63456cf8a47d5fadc05771b79aa3c2
JoGMG <joattah3@gmail.com>
2023-09-07 22:40:50 -0700

    email post request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,12 +5,11 @@
     - Using the `requests` package.
     - The email must be sent in the variable `email`
 """
-import sys
+from sys import argv
 import requests
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
+    url = argv[1]
+    value = {"email": argv[2]}
     r = requests.post(url, data=value)
     print(r.text)
-
