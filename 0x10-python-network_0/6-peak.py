035b998c8e47b67765bdcb7f821735b1de7dacbe
JoGMG <joattah3@gmail.com>
2023-09-08 17:28:39 +0100

    peak value

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,20 +4,29 @@
 
 def find_peak(list_of_integers):
     """ A function that finds a peak in a list of unsorted integers. """
-
-    if list_of_integers == []:
-        return None
+    
     d_list = list_of_integers
-    length = len(d_list)
-    index = int(length / 2)
+    if d_list is None or d_list == []:
+        return None
+    
+    l_len = len(d_list)
+    mid_index = int(l_len / 2)
 
-    if d_list[index - 1] < d_list[index] > d_list[index + 1]:
-        return d_list[index]
-    if d_list[index] == d_list[index - 1] == d_list[index + 1]:
-        return d_list[index] if index < 2 else find_peak(d_list[:index])
-    if index == 1 and d_list[index] < d_list[index - 1]:
-        return d_list[index - 1]
+    if l_len == 1:
+        return d_list[0]
+    if l_len == 2:
+        return max(d_list)
+    if d_list[mid_index - 1] <= d_list[mid_index] >= d_list[mid_index + 1]:
+        return d_list[mid_index]
 
-    if d_list[index + 1] > d_list[index - 1]:
-        return find_peak(d_list[index:])
-    return find_peak(d_list[:index])
+    if mid_index > 0 and d_list[mid_index] < d_list[mid_index + 1]:
+        return find_peak(d_list[mid_index:])
+    if mid_index > 0 and d_list[mid_index] < d_list[mid_index - 1]:
+        return find_peak(d_list[:mid_index])
+
+print(find_peak([1, 2, 4, 6, 3]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
+print(find_peak([2, 2, 2]))
+print(find_peak([]))
+print(find_peak([-2, -4, 2, 1]))
+print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
