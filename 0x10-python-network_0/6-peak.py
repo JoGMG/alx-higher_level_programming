1db1e037d79e5cc19a64cceae139ac269cfccd11
JoGMG <joattah3@gmail.com>
2023-09-08 17:31:22 +0100

    checker

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,11 +4,11 @@
 
 def find_peak(list_of_integers):
     """ A function that finds a peak in a list of unsorted integers. """
-    
+
     d_list = list_of_integers
     if d_list is None or d_list == []:
         return None
-    
+
     l_len = len(d_list)
     mid_index = int(l_len / 2)
 
@@ -23,10 +23,4 @@ def find_peak(list_of_integers):
         return find_peak(d_list[mid_index:])
     if mid_index > 0 and d_list[mid_index] < d_list[mid_index - 1]:
         return find_peak(d_list[:mid_index])
-
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
+    
