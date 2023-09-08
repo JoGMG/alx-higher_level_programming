08d249aea242cfb4b92eb7b624db26a69bd7ea01
JoGMG <joattah3@gmail.com>
2023-09-08 05:04:27 +0100

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,16 +5,15 @@ Usage: ./2-post_email.py <URL> <email>
 """
 
 
-import sys
+from sys import argv
 import urllib.parse
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
+    url = argv[1]
+    value = {"email": argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
-
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
         print(response.read().decode("utf-8"))
